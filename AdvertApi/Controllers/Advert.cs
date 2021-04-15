0b431c20eeb12dc9e4385b965240f3df3c99942b
Alf <alvmarcoux@gmail.com>
2021-04-15 14:10:05 -0500

    Build Advert Api

---
+++ AdvertApi/Controllers/Advert.cs
@@ -0,0 +1,69 @@
+﻿using AdvertApi.Models;
+using AdvertApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvertApi.Controllers
+{
+    [ApiController]
+    [Route("adverts/v1")]
+    public class Advert : ControllerBase
+    {
+        private readonly IAdvertStorageService _advertStorageService;
+
+        public Advert(IAdvertStorageService advertStorageService)
+        {
+            _advertStorageService = advertStorageService;
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(201, Type = typeof(CreateAdvertResponse))]
+        public async Task<IActionResult> Create(AdvertModel model)
+        {
+            string recordId;
+            try
+            {
+                recordId = await _advertStorageService.Add(model);
+            }
+            catch(KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+            return StatusCode(201, new CreateAdvertResponse() { Id = recordId});
+
+            //return Ok("");
+        }
+
+        [HttpPut]
+        [Route("Confirm")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200)]
+        public async Task<IActionResult> Confirm(ConfirmAdvertModel model)
+        {
+            try
+            {
+                await _advertStorageService.Confirm(model);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+            return new OkResult();
+
+        } 
+    }
+}
